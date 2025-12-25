981844d2557eea2aac43f190eef01ea6f1e35b79
Tornike Kapanadze <tornikekapan@gmail.com>
2025-12-25 10:47:01 +0200

    restore code

--- android/app/src/main/java/com/noxtton/pearpass/NativeClipboardModule.kt
+++ android/app/src/main/java/com/noxtton/pearpass/NativeClipboardModule.kt
@@ -1,4 +1,4 @@
-package com.pears.pass
+package com.noxtton.pearpass
 
 import android.app.AlarmManager
 import android.app.PendingIntent
@@ -171,7 +171,7 @@ class NativeClipboardModule(reactContext: ReactApplicationContext) : ReactContex
             val context = reactApplicationContext
             val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-            val intent = Intent("com.pears.pass.CLEAR_CLIPBOARD").apply {
+            val intent = Intent("com.noxtton.pearpass.CLEAR_CLIPBOARD").apply {
                 setPackage(context.packageName)
                 putExtra("text_to_match", text)
             }
@@ -211,7 +211,7 @@ class NativeClipboardModule(reactContext: ReactApplicationContext) : ReactContex
             val context = reactApplicationContext
             val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-            val intent = Intent("com.pears.pass.CLEAR_CLIPBOARD").apply {
+            val intent = Intent("com.noxtton.pearpass.CLEAR_CLIPBOARD").apply {
                 setPackage(context.packageName)
             }
 
